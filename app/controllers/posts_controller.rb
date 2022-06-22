06eb750ecc7babe4d1983294ddac49f1342667ab
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-06-22 16:35:11 +0800

    post user default to current

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def post_params
-    params.require(:post).permit(:user_id, :content, :status, :tags)
+    params.require(:post).permit(:user_id, :content, :status, :tags).merge(user_id: current_user.id)
   end
 end
