16c40d85b333ec33904bb8e0d53b546fdb016b14
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-06-24 15:20:34 +0800

    posts can be public or private, visibility exists

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.select { |post| post.visible_to? current_user }
   end
 
   # GET /posts/1 or /posts/1.json
