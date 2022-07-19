0d1930e1d7cdf6a9593388a4f0dee7626e38e273
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-07-19 13:46:28 +0800

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_718_015_200) do
+ActiveRecord::Schema[7.0].define(version: 20_220_719_054_527) do
   create_table 'action_text_rich_texts', force: :cascade do |t|
     t.string 'name', null: false
     t.text 'body'
@@ -67,6 +67,8 @@ ActiveRecord::Schema[7.0].define(version: 20_220_718_015_200) do
     t.datetime 'remember_created_at'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.string 'name', default: 'Anonymous', null: false
+    t.integer 'xp', default: 0, null: false
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
