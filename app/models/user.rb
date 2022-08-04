0d5a1e997007b98b1004af27102f9070e4d6a610
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-08-04 22:00:06 +0800

    add user page and link

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def path
+    "/users/#{id}"
+  end
+
+  def edit_info_path
+    '/users/edit_info'
+  end
+
+  def edit_path
+    '/users/edit'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     sessions: 'user/sessions',
     unlocks: 'user/unlocks'
   }
+  get '/users/edit_info', to: 'users#edit_info'
+  get '/users/:id', to: 'users#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
