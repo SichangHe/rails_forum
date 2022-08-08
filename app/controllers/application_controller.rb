ab5d8de59c3e3cda8c909dd2687c6385b8e7cf91
Steven Hé (Sīchàng) <84777573+SichangHe@users.noreply.github.com>
2022-08-08 11:17:38 +0800

    Added tags (#13)
    Tags input can be separated by space or commas.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :set_paper_trail_whodunnit
+  ActsAsTaggableOn.force_lowercase = true
+  ActsAsTaggableOn.delimiter = [',', ' ', "\n"]
 end
