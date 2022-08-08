a674486568a2c4eba84dad94c1f2eec78daa0f65
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-08-08 11:05:52 +0800

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_807_070_433) do
+ActiveRecord::Schema[7.0].define(version: 20_220_808_025_861) do
   create_table 'action_text_rich_texts', force: :cascade do |t|
     t.string 'name', null: false
     t.text 'body'
@@ -53,11 +53,43 @@ ActiveRecord::Schema[7.0].define(version: 20_220_807_070_433) do
   create_table 'posts', force: :cascade do |t|
     t.integer 'user_id', null: false
     t.integer 'status', default: 0
-    t.text 'tags'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.string 'title', default: 'Untitled', null: false
+    t.string 'tags'
     t.index ['user_id'], name: 'index_posts_on_user_id'
+  end
+
+  create_table 'taggings', force: :cascade do |t|
+    t.integer 'tag_id'
+    t.string 'taggable_type'
+    t.integer 'taggable_id'
+    t.string 'tagger_type'
+    t.integer 'tagger_id'
+    t.string 'context', limit: 128
+    t.datetime 'created_at', precision: nil
+    t.string 'tenant', limit: 128
+    t.index ['context'], name: 'index_taggings_on_context'
+    t.index %w[tag_id taggable_id taggable_type context tagger_id tagger_type], name: 'taggings_idx',
+                                                                                unique: true
+    t.index ['tag_id'], name: 'index_taggings_on_tag_id'
+    t.index %w[taggable_id taggable_type context], name: 'taggings_taggable_context_idx'
+    t.index %w[taggable_id taggable_type tagger_id context], name: 'taggings_idy'
+    t.index ['taggable_id'], name: 'index_taggings_on_taggable_id'
+    t.index %w[taggable_type taggable_id], name: 'index_taggings_on_taggable_type_and_taggable_id'
+    t.index ['taggable_type'], name: 'index_taggings_on_taggable_type'
+    t.index %w[tagger_id tagger_type], name: 'index_taggings_on_tagger_id_and_tagger_type'
+    t.index ['tagger_id'], name: 'index_taggings_on_tagger_id'
+    t.index %w[tagger_type tagger_id], name: 'index_taggings_on_tagger_type_and_tagger_id'
+    t.index ['tenant'], name: 'index_taggings_on_tenant'
+  end
+
+  create_table 'tags', force: :cascade do |t|
+    t.string 'name', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'taggings_count', default: 0
+    t.index ['name'], name: 'index_tags_on_name', unique: true
   end
 
   create_table 'users', force: :cascade do |t|
@@ -104,4 +136,5 @@ ActiveRecord::Schema[7.0].define(version: 20_220_807_070_433) do
   add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
   add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
   add_foreign_key 'posts', 'users'
+  add_foreign_key 'taggings', 'tags'
 end
