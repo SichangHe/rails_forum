3994ee4d463e35ddc4bb0feca9336d891d6cf90d
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-08-09 13:02:39 +0800

    reorder posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all.joins(:user).select { |post| post.visible_to? current_user }
+    @posts = Post.order(created_at: :desc).limit(16).joins(:user).select { |post| post.visible_to? current_user }
   end
 
   # GET /posts/1 or /posts/1.json
