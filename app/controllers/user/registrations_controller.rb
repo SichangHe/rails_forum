377fd70410b8bfd7a3ec1b5d6c688f9a2800c0b0
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-08-10 11:41:46 +0800

    basic avatar support

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -47,7 +47,7 @@ class User::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[attribute name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[attribute])
   end
 
   # The path used after sign up.
