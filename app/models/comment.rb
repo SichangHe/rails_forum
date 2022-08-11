2d4335ca5e089ae2973891eeb8f5b6b31d0cc201
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-08-11 22:05:10 +0800

    comment to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,6 +8,26 @@ class Comment < ApplicationRecord
 
   def mutable_to?(user1)
     # TODO: admin user
-    user == user1
+    user1 && user == user1
   end
+
+  # Path to create new comment to comment
+  def new_comments_path
+    "/comments/Comment/#{id}/new"
+  end
+
+  # Path to show comments of the comment
+  def comments_path
+    "/comments/Comment/#{id}"
+  end
+end
+
+# DOM id for comment used in index page
+def comment_dom_id(type, id, offset)
+  "#{type}#{id}comments#{offset}offset"
+end
+
+# DOM id for new comment
+def new_comment_dom_id(type, id)
+  "#{type}#{id}new_comments"
 end
