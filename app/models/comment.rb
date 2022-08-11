b7b37bdd324fbcc6f0e6416ce46896cfd849beb9
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-08-11 11:03:28 +0800

    add rich_text,paper_trial,vote to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentables
+  has_rich_text :content
+  has_paper_trail
+  acts_as_votable
 end
