9935a1510dc647fafbc50399fa7b99d859f4045c
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-08-12 16:12:02 +0800

    prevent notice after edit comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -57,7 +57,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to comment_url(@comment), notice: 'Comment was successfully updated.' }
+        format.html { redirect_to comment_url(@comment) }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit, status: :unprocessable_entity }
